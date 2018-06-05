60a4ec8a6c1bbf0e415f388a77dab27a2f3eab40
rusleysantos <rusley_silva@hotmail.com>
2018-06-05 16:19:29 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <stdbool.h>
 #define TAM 3
 
-int Codigo[TAM], Estoque[TAM], Vendido[TAM],i,Opcao;
+int Codigo[TAM], Estoque[TAM], Vendido[TAM],i,j,Opcao, tes=1, Aux,AuxMenu;
 float PrecoCusto[TAM], PrecoVenda[TAM];
+char Nome[TAM][100];
 
 int main(){
     setlocale(LC_ALL, "");
     PrincipalMenu();
+}
+
+bool Valor(int Codigo1)
+{
+   bool flag = false;
+
+    for(j=0;j<TAM;j++)
+    {
+        if(Codigo[j] == Codigo1)
+        {
+            flag = true;
+            printf("O código %i já foi utilizado, digite outro! \n", Codigo[j]);
+        }
+    }
+    return flag;
 }
 
 int Logica(Opcao){
@@ -16,20 +33,29 @@ int Logica(Opcao){
     {
         case 1:
             system("cls");
-            for(i=0;i<TAM;i++)
+            for(i=0;i<=TAM;i++)
             {
-                printf("Digite o código do produto \n");
-                scanf("%i",&Codigo[i]);
+                do{
+                    printf("Digite o código do produto: \n");
+                     scanf("%i",&Aux);
+                    }
+                    while(Valor(Aux));
+                   Codigo[i]=Aux;
                 printf("");
-                printf("Digite a quantidade em estoque \n");
+                printf("Digite o nome do produto: \n");
+                scanf("%s",&Nome[i][100]);
+                printf("");
+                printf("Digite a quantidade em estoque: \n");
                 scanf("%i",&Estoque[i]);
                 printf("");
-                printf("Digite a quantidade vendida \n");
+                printf("Digite a quantidade vendida: \n");
                 scanf("%i",&Vendido[i]);
                 printf("------------------------------\n");
             }
-
-
+            printf("Todos os produtos foram cadastrados com sucesso!\n");
+            printf("Deseja voltar ao menu principal?(1 = sim / 2 = não) \n");
+            scanf("%i",&AuxMenu);
+            VoltarMenu(AuxMenu);
             break;
 
         case 2:
@@ -74,4 +100,22 @@ int Logica(Opcao){
     scanf("%i",&Opcao);
     Logica(Opcao);
     return Opcao;
+    }
+
+    int VoltarMenu(AuxMenu){
+
+        switch(AuxMenu)
+        {
+        case 1:
+            system("cls");
+            PrincipalMenu();
+            break;
+        case 2:
+            exit(0);
+            break;
+        default:
+            exit(0);
+            break;
+
+        }
     }
