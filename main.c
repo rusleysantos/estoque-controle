0efaf7cc942e4ea1150e4444cefb07d73934099e
rusleysantos <rusley_silva@hotmail.com>
2018-06-06 17:36:42 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,17 +2,19 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <stdbool.h>
-#define TAM 3
 
-int Codigo[TAM], Estoque[TAM], Vendido[TAM],i,j,Opcao, tes=1, Aux,AuxMenu;
+#define TAM 1
+
+int Codigo[TAM], Estoque[TAM], Vendido[TAM],i,j,Opcao, tes=1, Aux=0,AuxMenu, flagCad=0,flagBusca=0, AuxBusca;
 float PrecoCusto[TAM], PrecoVenda[TAM];
 char Nome[TAM][100];
 
-int main(){
+int main()
+{
     setlocale(LC_ALL, "");
     PrincipalMenu();
 }
-
+//Verifica se o ID e igual a algum já cadastrado
 bool Valor(int Codigo1)
 {
    bool flag = false;
@@ -32,6 +34,7 @@ int Logica(Opcao){
     switch(Opcao)
     {
         case 1:
+            //Cadatrar
             system("cls");
             for(i=0;i<=TAM;i++)
             {
@@ -40,18 +43,31 @@ int Logica(Opcao){
                      scanf("%i",&Aux);
                     }
                     while(Valor(Aux));
-                   Codigo[i]=Aux;
-                printf("");
+                    Codigo[i]=Aux;
+
                 printf("Digite o nome do produto: \n");
                 scanf("%s",&Nome[i][100]);
-                printf("");
+                fflush(stdin);
+
                 printf("Digite a quantidade em estoque: \n");
                 scanf("%i",&Estoque[i]);
-                printf("");
+
                 printf("Digite a quantidade vendida: \n");
                 scanf("%i",&Vendido[i]);
+
+                printf("Digite o preço de venda: \n");
+                scanf("%f",&PrecoVenda[i]);
+                fflush(stdin);
+                printf("%f",PrecoVenda[i]);
+
+                printf("Digite o preço de custo: \n");
+                scanf("%f",&PrecoCusto[i]);
+                fflush(stdin);
+
+
                 printf("------------------------------\n");
             }
+            flagCad = 1;
             printf("Todos os produtos foram cadastrados com sucesso!\n");
             printf("Deseja voltar ao menu principal?(1 = sim / 2 = não) \n");
             scanf("%i",&AuxMenu);
@@ -59,8 +75,38 @@ int Logica(Opcao){
             break;
 
         case 2:
+            //Alterar
             system("cls");
-            //logica
+
+            printf("Digite o ID do produto  que deseja alterar:\n");
+            scanf("%i",&AuxBusca);
+
+            for(i=0;i<=TAM;i++)
+            {
+                if(Codigo[i]==AuxBusca)
+                {
+                    printf("Digite a quantidade em estoque: \n");
+                    scanf("%i",&Estoque[i]);
+
+                    printf("Digite o preço de venda: \n");
+                    scanf("%f",&PrecoVenda[i]);
+                    fflush(stdin);
+
+                    flagBusca = 1;
+
+                    printf("Deseja voltar ao menu principal?(1 = sim / 2 = não) \n");
+                    scanf("%i",&AuxMenu);
+                    VoltarMenu(AuxMenu);
+                }
+            }
+              if(flagBusca==0)
+                {
+                    printf("Não foi encontrato nenhum produto com esse ID.\n");
+                    printf("");
+                    printf("Deseja voltar ao menu principal?(1 = sim / 2 = não) \n");
+                    scanf("%i",&AuxMenu);
+                    VoltarMenu(AuxMenu);
+                }
             break;
 
         case 3:
@@ -68,20 +114,71 @@ int Logica(Opcao){
             //logica
 
         case 4:
+            //Exibir todos
             system("cls");
-            //logica
+            if(flagCad != 0)
+            {
+                for(i=0;i<=TAM;i++)
+                {
+                printf("ID: %i\n",Codigo[i]);
+                printf("Nome: %s\n",&Nome[i][100]);
+                printf("Estoque: %i\n",Estoque[i]);
+                printf("Vendido: %i\n",Vendido[i]);
+                printf("Preço de custo: %.2f\n",PrecoVenda[i]);
+                printf("Preco de Venda: %.2f\n",PrecoCusto[i]);
+                printf("------------------------------\n");
+                }
+                printf("");
+                printf("Deseja voltar ao menu principal?(1 = sim / 2 = não) \n");
+                scanf("%i",&AuxMenu);
+                VoltarMenu(AuxMenu);
+            }
+            else
+            {
+                printf("Você precisa cadastrar produtos!!!\n");
+                printf("");
+                printf("Deseja voltar ao menu principal?(1 = sim / 2 = não) \n");
+                scanf("%i",&AuxMenu);
+                VoltarMenu(AuxMenu);
+            }
 
         case 5:
+            //Consultar por ID
             system("cls");
-            //logica
+            printf("Digite o código do produto\n");
+            scanf("%i",&AuxBusca);
+            for(i=0;i<=TAM;i++)
+            {
+                if(Codigo[i]==AuxBusca)
+                {
+                    printf("------------------------------\n");
+                    printf("ID: %i\n",Codigo[i]);
+                    printf("Nome: %s\n",&Nome[i][100]);
+                    printf("Estoque: %i\n",Estoque[i]);
+                    printf("Vendido: %i\n",Vendido[i]);
+                    printf("------------------------------\n");
+                    flagBusca = 1;
 
+                    printf("Deseja voltar ao menu principal?(1 = sim / 2 = não) \n");
+                    scanf("%i",&AuxMenu);
+                    VoltarMenu(AuxMenu);
+                }
+            }
+            if(flagBusca==0)
+            {
+                printf("Não foi encontrato nenhum produto com esse ID.\n");
+                printf("");
+                printf("Deseja voltar ao menu principal?(1 = sim / 2 = não) \n");
+                scanf("%i",&AuxMenu);
+                VoltarMenu(AuxMenu);
+            }
+            break;
         case 6:
-            system("cls");
-            //logica
-
-            default:
-                exit(0);
-                break;
+            exit(0);
+            break;
+        default:
+            exit(0);
+            break;
         }
     }
 
@@ -119,3 +216,5 @@ int Logica(Opcao){
 
         }
     }
+
+
